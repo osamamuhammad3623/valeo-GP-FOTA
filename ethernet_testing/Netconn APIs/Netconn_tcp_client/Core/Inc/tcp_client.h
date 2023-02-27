fa014c0edd2434cda94b1f9cb0f286602fd22b75
Kyrillos Sawiris <kyrillos.sawiris99@gmail.com>
2023-02-27 23:53:34 +0200

    netconn tcp client V1

---
+++ ethernet_testing/Netconn APIs/Netconn_tcp_client/Core/Inc/tcp_client.h
@@ -0,0 +1,14 @@
+/*
+ * tcp_client.h
+ *
+ *  Created on: Feb 24, 2023
+ *      Author: phelp
+ */
+
+#ifndef INC_TCP_CLIENT_H_
+#define INC_TCP_CLIENT_H_
+
+void tcpclient_init (void);
+
+
+#endif /* INC_TCP_CLIENT_H_ */
