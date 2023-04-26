0d91ae7d152cd82bf30c93106313775c90907af4
Osama Muhammad <59265090+osamamuhammad3623@users.noreply.github.com>
2023-04-26 19:06:10 +0200

    Uploading to Firebase is done.

---
+++ oem_application/connect_to_firebase.py
@@ -0,0 +1,19 @@
+import pyrebase
+
+firebaseConfig = {
+  "apiKey": "AIzaSyBQ1Q2OPmZXkDQgMNkyeaV-FwdAR-Kphqc",
+  "authDomain": "oem-application.firebaseapp.com",
+  "databaseURL": "https://oem-application-default-rtdb.firebaseio.com",
+  "projectId": "oem-application",
+  "storageBucket": "oem-application.appspot.com",
+  "messagingSenderId": "526497884209",
+  "appId": "1:526497884209:web:3c98198729aac226638d10",
+  "measurementId": "G-LFP70JP2X8",
+
+  "serviceAccount" : "serviceAccountKey.json"
+};
+
+firebase = pyrebase.initialize_app(firebaseConfig)
+storage = firebase.storage()
+
+storage.child("Latest_Package/test_app.bin").put("test_app.bin")
