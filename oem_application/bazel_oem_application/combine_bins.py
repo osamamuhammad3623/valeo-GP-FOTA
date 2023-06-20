a0bfc995cfdc521a967b28937b730fa10d5ac623
Abdelrahman Elsayed Mohamed <60861577+abdelrahman99999@users.noreply.github.com>
2023-06-20 13:52:15 +0300

    Update combine_bins.py

--- oem_application/bazel_oem_application/combine_bins.py
+++ oem_application/bazel_oem_application/combine_bins.py
@@ -2,7 +2,7 @@
 #define SB_CERT_ADDRESS 			    (APPLICATION_EXENSIONS_ADDRESS + 0x0800)
 #define META_DATA_ADDRESS               (APPLICATION_EXENSIONS_ADDRESS + 0x01000)
 #define SIGNATURE_ADDRESS               (APPLICATION_EXENSIONS_ADDRESS + 0x01400)
-#define CERTIFICATES_METADATA_ADDRESS   (APPLICATION_EXENSIONS_ADDRESS + 0x01600)
+#define CERTIFICATES_METADATA_ADDRESS   (APPLICATION_EXENSIONS_ADDRESS + 0x01602)
 
 
 def combine_security_files(img_num):
@@ -26,5 +26,5 @@ def combine_security_files(img_num):
             f1.write(data)
         with open("certificates_metadata.bin", "rb") as f2:
             data=f2.read()
-            f1.seek(0x01600)
+            f1.seek(0x01602)
             f1.write(data)
